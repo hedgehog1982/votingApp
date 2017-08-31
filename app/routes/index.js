817f32d02ea9caea4525ead0a142d2c8a0a3fea5
Wayne Williamson <hedgehog1982@gmail.com>
2017-08-31 15:36:50 +0000

    removed all clementine un-needed routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,19 +5,13 @@ var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+	
 
 	var clickHandler = new ClickHandler();
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
+		.get(function (req, res) {
+			res.sendFile(path + '/public/login.html');
 		});
 
 	app.route('/login')
@@ -25,21 +19,10 @@ module.exports = function (app, passport) {
 			res.sendFile(path + '/public/login.html');
 		});
 
-	app.route('/logout')
+	app.route('/auth/twitter')  //for authorising twitter // not sure how to do this yet
 		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+			res.send("clicked");
+		});	
 
 	app.route('/auth/github')
 		.get(passport.authenticate('github'));
@@ -50,8 +33,5 @@ module.exports = function (app, passport) {
 			failureRedirect: '/login'
 		}));
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+
 };
