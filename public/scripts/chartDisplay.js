82fb48ab529fa186d839822bfd448c406e312a7e
Wayne Williamson <hedgehog1982@gmail.com>
2017-09-04 17:58:55 +0000

    started to do chart display and validation

--- public/scripts/chartDisplay.js
+++ public/scripts/chartDisplay.js
@@ -1,29 +1,25 @@
 /*global google*/
 /*global $*/
     
-      google.charts.load('current', {'packages':['corechart']});
+      google.charts.load('current', {'packages':['corechart']});  //for external google chart
       google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
       
-        var newArray = [["Selection", "People"]];  //putting in a header
-        console.log("NEW aRRAY IS ", newArray);
+        var newArray = [["Selection", "People"]];  //putting in a header  //our header for charts
+       var datavalues = $("#pieSections").val().split('\n').filter(function(entry) { //read in data ignore spaces
+            return entry.trim() != '';
+            });
+       
 
-        var datavalues = $("#pieSections").val().split('\n');
-                console.log("data values is ", datavalues);
     
         for (var i=0 ; i < datavalues.length; i++){
-             newArray.push([datavalues[i] , 1]);
+             newArray.push([datavalues[i] , 1]);  //push with a value of one to display the chart
         }
-        
-        
-        console.log(datavalues);
+      
+        var data = google.visualization.arrayToDataTable(newArray); // save the data to be passed to draw function
 
-        var data = google.visualization.arrayToDataTable(newArray);
-
-        var options = {
-          title: $("#chartTitle").val()
-        };
+        var options = { title: $("#chartTitle").val() };  //for the chart title
         
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
@@ -37,17 +33,24 @@
     (drawChart());  // if we type anything in the fields we update the pie chart!
     });
     
-   $('#pieSections').keyup(function(){
-     drawChart();
-   });
+     $('#pieSections').keyup(function(){
+       drawChart();
+       });
    
      $("#submit").click(function(event) {   //data checking! is it valid? is it enough?
-    event.preventDefault(); //needed to stop html button deafult i think
-    if ($("#chartTitle").val().length === 0  ){
-              var datavalues = $("#pieSections").val().split('\n').filter("");
-      console.log("lenght is ", datavalues.length);
-      window.alert("No Chart Title Entered");
-    }
+       event.preventDefault(); //needed to stop html button deafult i think
+        var datavalues = $("#pieSections").val().split('\n').filter(function(entry) { 
+            return entry.trim() != '';
+            });
+            ; //read in from text area
+        console.log("data in text field is ", datavalues)
+        
+        if ($("#chartTitle").val().length === 0  ){
+              console.log("lenght is ", datavalues.length);
+              window.alert("No Chart Title Entered");  //swap this for bootstrap warning
+              } else {
+                
+              }
      });
 
     
